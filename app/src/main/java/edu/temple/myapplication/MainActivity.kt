726b166f665b68ab9d6d7e1d8c7a5caa10278a9c
Jun Zhou <tun40202@temple.edu>
2023-11-09 11:51:49 -0500

    binderservice created

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,25 +1,47 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+        lateinit var timerBinder : TimerService.TimerBinder
+        var isconnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val serviceConnection = object: ServiceConnection{
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                timerBinder = service as TimerService.TimerBinder
+                isconnected= true
+            }
+            override fun onServiceDisconnected(name: ComponentName?) {
+                isconnected = false
+            }
+        }
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
         findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            if(isconnected) timerBinder.start(50)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if(isconnected) timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if(isconnected) timerBinder.stop()
         }
     }
 }
