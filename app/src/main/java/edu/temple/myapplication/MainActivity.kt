7c89832feffe435aad77efc55b759281c35d36eb
Jun Zhou <tun40202@temple.edu>
2023-11-28 12:11:01 -0500

    depulicate error, temporary commit

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -6,6 +6,8 @@ import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -25,23 +27,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         bindService(
             Intent(this, TimerService::class.java),
             serviceConnection,
             BIND_AUTO_CREATE
         )
         findViewById<Button>(R.id.startButton).setOnClickListener {
-            if(isconnected) timerBinder.start(50)
+
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-            if(isconnected) timerBinder.pause()
+
         }
-        
+
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-            if(isconnected) timerBinder.stop()
+
         }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var flag : Boolean = false
+        while(!flag){
+        when(item.itemId){
+            R.id.startButton -> {if(isconnected) timerBinder.start(50)
+                flag= true}
+            R.id.stopButton ->  {if(isconnected) timerBinder.stop()
+                flag=true}
+            R.id.pauseButton-> {if(isconnected) timerBinder.pause()
+                flag=true}
+        }}
+        return super.onOptionsItemSelected(item)
     }
 }
